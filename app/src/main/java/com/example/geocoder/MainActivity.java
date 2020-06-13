7c4de58115273561139cb8504b46f981f15e815d
samuray0081 <samuray0081@yahoo.com>
2020-06-13 14:39:49 +1000

    Geocoder

--- app/src/main/java/com/example/geocoder/MainActivity.java
+++ app/src/main/java/com/example/geocoder/MainActivity.java
@@ -1,51 +1,67 @@
 package com.example.geocoder;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.map.CameraListener;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CameraUpdateSource;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.mapview.MapView;
 import org.xml.sax.SAXException;
-
 import java.io.IOException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
 
-    YandexGeoCoder ya = new YandexGeoCoder();
-    TextView txt;
+public class MainActivity extends AppCompatActivity implements CameraListener {
+
+    MapView mapView;
     Button getButton;
-    String rsp;
+    EditText searchEdit;
+    private final String MAPKIT_API_KEY = "48fafd2b-6dbe-4eec-9216-688d45b7595c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.setLocale("RU_ru");
+        MapKitFactory.initialize(this);
 
         setContentView(R.layout.activity_main);
-        txt = (TextView) findViewById(R.id.textView);
+        super.onCreate(savedInstanceState);
 
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().addCameraListener(this);
+
+        searchEdit = (EditText)findViewById(R.id.search_edit);
         getButton = (Button) findViewById(R.id.button);
     }
 
-
     public void onGetClick(View view) throws IOException, ParserConfigurationException, SAXException {
-
+        YandexGeoCoder ya = new YandexGeoCoder(mapView, searchEdit.getText().toString());
+        ya.GET();
 
     }
+
+    @Override
+    public void onCameraPositionChanged(@NonNull Map map, @NonNull CameraPosition cameraPosition, @NonNull CameraUpdateSource cameraUpdateSource, boolean b) {
+
+    }
+
+    @Override
+    protected void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
 }
--- app/src/main/java/com/example/geocoder/YandexGeoCoder.java
+++ app/src/main/java/com/example/geocoder/YandexGeoCoder.java
@@ -1,63 +1,70 @@
 package com.example.geocoder;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.fluent.Request;
-
+import android.app.Activity;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Collection;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Response;
 
-public class YandexGeoCoder {
-
+public class YandexGeoCoder extends Activity {
 
     final String apikey = "a817921e-3bf3-4517-8504-37fc2b1d334a";
     private String answer;
+    private MapView mapView;
+    private Coords coords;
+    private String request;
 
-    public String POST(String url, Collection<NameValuePair> params) throws IOException {
-
-        /*final Collection<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("title", "foo"));
-        params.add(new BasicNameValuePair("body", "bar"));
-        params.add(new BasicNameValuePair("userId", "1"));*/
-
-        final Content postResultForm = Request.Post(url)
-                .bodyForm(params, Charset.defaultCharset())
-                .execute().returnContent();
-        System.out.println(postResultForm.asString());
-        return "";
+    public YandexGeoCoder(MapView mapView, String request)  {
+        this.mapView = mapView;
+        this.request = ConvertRequest(request);
     }
 
+    private String ConvertRequest(String request) {
+        return request.replace(' ', '+');
+    }
 
-
-    public String GET(String url) throws IOException {
-
+    public String GET() throws IOException {
         OkHttpClient client = new OkHttpClient();
         okhttp3.Request request = new okhttp3.Request.Builder()
-                .url("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+                .url("https://geocode-maps.yandex.ru/1.x/?apikey="+apikey+"&format=xml&geocode="+this.request)
                 .build();
-
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
                 e.printStackTrace();
             }
-
             @Override
             public void onResponse(Call call, final Response response) throws IOException {
                 if (!response.isSuccessful()) {
                     throw new IOException("Unexpected code " + response);
                 } else {
                     answer = response.body().string();
+
+                    Pattern pattern = Pattern.compile("<pos>(.+?)</pos>");
+                    Matcher matcher = pattern.matcher(answer);
+                    if (matcher.find()) {
+                        coords = new Coords(answer.substring(matcher.start() + 5, matcher.end() - 6));
+                        Thread t = new Thread(new Runnable() {
+                            public void run() {
+                                runOnUiThread(runn1);
+                            }
+                        });
+                        t.start();
+                    }
                 }
             }
         });
         return answer;
     }
+    Runnable runn1 = new Runnable() {
+        public void run() {
+            mapView.getMap().move(new CameraPosition(coords.getPoint(), 16.0f, 0.0f, 0.0f));
+        }
+    };
 
 }
